3c890f5cd24398984a5ddaf227faad94d059da96
Sander.Sinijarv <sander.sinijarv@gmail.com>
2019-02-20 14:53:15 +0200

    Replace deprecated variable

--- DrinkDispenser/DrinkDispenser.ino
+++ DrinkDispenser/DrinkDispenser.ino
@@ -1,18 +1,3 @@
-/*
- UDPSendReceiveString:
- This sketch receives UDP message strings, prints them to the serial port
- and sends an "acknowledge" string back to the sender
-
- A Processing sketch is included at the end of file that can be used to send
- and received messages for testing with a computer.
-
- created 21 Aug 2010
- by Michael Margolis
-
- This code is in the public domain.
- */
-
-
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
@@ -86,7 +71,7 @@ void loop() {
     Serial.println("Contents:");
     Serial.println(packetBuffer);
     
-    if(str == wordCompare) {
+    if(packetToString == wordCompare) {
       digitalWrite(relay1Pin, HIGH);
       digitalWrite(relay2Pin, HIGH);
     }
