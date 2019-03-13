218788345ec9a1c0e10b64d5845cf9844ff57b00
Sander.Sinijarv <sander.sinijarv@gmail.com>
2019-03-13 22:40:34 +0200

    Move Ethernet startup to separate function

--- DrinkDispenser.ino
+++ DrinkDispenser.ino
@@ -24,28 +24,7 @@ void setup() {
   pinMode(relay1Pin, OUTPUT);
   pinMode(relay2Pin, OUTPUT);
   
-  // start the Ethernet
-  Ethernet.begin(mac, ip);
-
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
-  // start UDP
-  Udp.begin(localPort);
+  startEthernet();
 }
 
 void loop() {
@@ -76,12 +55,38 @@ void loop() {
       digitalWrite(relay2Pin, HIGH);
     }
 
+    /*Commented out, because it crashes router*/
     // send a reply to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(ReplyBuffer);
-    Udp.endPacket();
+    // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    // Udp.write(ReplyBuffer);
+    // Udp.endPacket();
   }
   delay(10);
+}
+
+void startEthernet() {
+    // start the Ethernet
+  Ethernet.begin(mac, ip);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start UDP
+  Udp.begin(localPort);
 }
 
 
